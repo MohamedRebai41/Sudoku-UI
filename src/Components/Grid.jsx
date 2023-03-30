8b11832711c3fcd720fdac022e010733e80e1269
root <root@Fankoush.localdomain>
2023-03-30 17:23:11 +0100

    undo button now can undo more than one step

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,43 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import checkValid from "../Functions/checkValid";
+import { useRef } from "react";
 import Row from "./Row";
 
 const Grid = (props) => {
-  const initialGrid = [
-    [9, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 7, 0, 0],
-    [0, 0, 4, 0, 0, 0, 0, 0, 8],
-    [0, 0, 0, 0, 0, 5, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 7, 0, 0, 6, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 9],
-  ];
-
   const caseRefs = useRef([]);
   for (let i = 0; i < 81; i++) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     caseRefs.current[i] = useRef(null);
   }
-
-  const [grid, setGrid] = useState(initialGrid);
-  const [lastGrid, setLastGrid] = useState(initialGrid);
-  useEffect(
-    () => {
-      setGrid(initialGrid);
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [props.clear]
-  );
-
-  useEffect(
-    () => {
-      setGrid(lastGrid);
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [props.undo]
-  );
 
   const handleArrowKey = (direction, index) => {
     let nextIndex = index;
@@ -55,28 +24,18 @@ const Grid = (props) => {
     }
   };
 
-  function handleCaseChange(row, col, value) {
-    const newGrid = JSON.parse(JSON.stringify(grid));
-    newGrid[row][col] = +value;
-    setLastGrid(grid);
-    setGrid(newGrid);
-  }
-
-  function checkValidCaseChange(row, col, value) {
-    return checkValid(row, col, value, grid);
-  }
   return (
     <div className="grid">
-      {grid.map((row, index) => (
+      {props.grid.map((row, index) => (
         <Row
           key={index}
           row={row}
           ref={caseRefs}
-          handleCaseChange={handleCaseChange}
+          handleCaseChange={props.handleCaseChange}
           isEdge={index % 3 == 2}
           index={index}
           handleArrowKey={handleArrowKey}
-          checkValid={checkValidCaseChange}
+          checkValid={props.checkValidCaseChange}
         ></Row>
       ))}
     </div>
--- src/Components/Puzzle.jsx
+++ src/Components/Puzzle.jsx
@@ -2,16 +2,56 @@ import Grid from "./Grid";
 import { FaUndo } from "react-icons/fa";
 import { AiOutlineClear } from "react-icons/ai";
 import { useState } from "react";
+import checkValid from "../Functions/checkValid";
 const Puzzle = () => {
-  const [clearTrigger, setClearTrigger] = useState(false);
-  const [undoTrigger, setUndoTrigger] = useState(false);
+  const initialGrid = [
+    [9, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 7, 0, 0],
+    [0, 0, 4, 0, 0, 0, 0, 0, 8],
+    [0, 0, 0, 0, 0, 5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 0, 0, 6, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 9],
+  ];
+  const undoLimit = 10;
+  const [grid, setGrid] = useState(initialGrid);
+  const [gridHistory, setGridHistory] = useState([]);
+  const [undoLimited, setUndoLimited] = useState(false);
+
+  function handleCaseChange(row, col, value) {
+    const newGrid = JSON.parse(JSON.stringify(grid));
+    newGrid[row][col] = +value;
+    const newGridHistory = gridHistory;
+    newGridHistory.push(grid);
+    if (newGridHistory.length > undoLimit) {
+      newGridHistory.shift();
+    }
+    if (newGridHistory.length == 1) {
+      setUndoLimited(false);
+    }
+    setGridHistory(newGridHistory);
+    setGrid(newGrid);
+  }
+
+  function checkValidCaseChange(row, col, value) {
+    return checkValid(row, col, value, grid);
+  }
+
   return (
     <div>
       <div className="puzzle-bar">
         <button
           className="undo button"
+          disabled={undoLimited}
           onClick={() => {
-            setUndoTrigger(!undoTrigger);
+            if (gridHistory.length > 0) {
+              setGrid(gridHistory.pop());
+            }
+            if (gridHistory.length == 0) {
+              setUndoLimited(true);
+            }
           }}
         >
           <FaUndo className="undo-icon" />
@@ -19,13 +59,17 @@ const Puzzle = () => {
         <button
           className="clear button"
           onClick={() => {
-            setClearTrigger(!clearTrigger);
+            setGrid(initialGrid);
           }}
         >
           <AiOutlineClear className="clear-icon" />
         </button>
       </div>
-      <Grid clear={clearTrigger} undo={undoTrigger} />
+      <Grid
+        grid={grid}
+        handleCaseChange={handleCaseChange}
+        checkValidCaseChange={checkValidCaseChange}
+      />
     </div>
   );
 };
